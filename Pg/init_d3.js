2622b249b689239436bc4a7c1c966418b6aae145
Jorim <johannestheuns@gmail.com>
2018-12-09 16:02:54 +0100

    Commit

---
+++ Pg/init_d3.js
@@ -0,0 +1,274 @@
+var darkGold = "#db8a00";
+var darkBlue = "#176ab7";
+var lightGold = "#efe8dc";
+var lightBlue = "#E0E7F2";
+
+var pgData;
+
+//human readable floats
+var f = d3.format(".1f");
+
+d3.json("data.json", function (error, json) {
+    if (error)
+        return console.warn(error);
+    pgData = json;
+    visualise();
+});
+
+function visualise() {
+
+    var scene = d3.select('a-scene');
+    var curveButton = d3.select('#curveButton');
+
+    var nodeData = pgData.graph.nodes;
+    var edgeData = pgData.graph.edges;
+
+    var curvyPaths = [];
+    var flatPaths = [];
+
+    var nodes = [];
+    var edges = [];
+
+    var shader = "flat";
+
+    var connectedHeads = [];
+    var connectedTails = [];
+
+    var depth = false;
+    var curve = false;
+
+    var lineColor = "#2F86DA";
+
+    //Set max dimensions of flat flow chart
+    let maxWidth = 1.9;
+    let maxHeight = 2.5;
+    let eyeHeight = 0;
+
+    //set max dimensions of curved flow chart (in degrees)
+    let maxTheta = 70;
+    let maxPhi = 90;
+    let radius = 1.5;
+
+    //Sets how far behind the nodes the edges will render in both flat and curved mode
+    let zshift = -0.05;
+
+    //not sure
+    let depthFactor = 0.3;
+    let animateScale = 1;
+
+    //sets the maximum complexity of the animation. Used to optimise FPS
+    let lineResolution = 20;
+    let showLabels = true;
+    let animateDepth = true;
+
+    //Function that takes nodeData, finds absolute max distance from center
+    //creates scale that aligns all data around centerpoint
+    let xShift = pgData.graph.centralpoint.x;
+    let xmax = d3.max(nodeData, function (d) {
+        return d.x
+    });
+    let xmin = d3.min(nodeData, function (d) {
+        return d.x
+    });
+    let xAbsMax = Math.max(Math.abs(xShift - xmax), Math.abs(xShift - xmin));
+    var xScale = d3.scaleLinear()
+        .domain([xShift, xShift + xAbsMax])
+        .range([0, maxWidth / 2]);
+
+    //almost same as xScale
+    //limits between 0 and maxheight
+    //inverts the y axis
+    let yShift = pgData.graph.centralpoint.y;
+    let ymax = d3.max(nodeData, function (d) {
+        return d.y
+    });
+    let ymin = d3.min(nodeData, function (d) {
+        return d.y
+    });
+    let yAbsMax = Math.max(Math.abs(yShift - ymax), Math.abs(yShift - ymin));
+    var yScale = d3.scaleLinear()
+        .domain([yShift + yAbsMax, yShift - yAbsMax])
+        .range([-maxHeight / 2, maxHeight / 2]);
+
+    //Scale for horisontal sweep angle (Phi)
+    var pScale = d3.scaleLinear()
+        .domain([xShift, xShift + xAbsMax])
+        .range([0, -maxPhi / 2])
+
+    //Scale for vertial elevation angle (Theta)
+    //Makes eyeHeight = 0
+    var tScale = d3.scaleLinear()
+        .domain([yScale.invert(eyeHeight), yScale.invert(eyeHeight) - yAbsMax])
+        .range([0, maxTheta / 2]);
+
+    //Simple Scale for Color
+    let cExtent = d3.extent(nodeData, function (d) {
+        return d.weight;
+    });
+
+    var colorBlue = d3.scaleLinear()
+        .domain(cExtent)
+        .interpolate(d3.interpolateHcl)
+        .range([d3.rgb(lightBlue), d3.rgb(darkBlue)]);
+
+    var colorGold = d3.scaleLinear()
+        .domain(cExtent)
+        .interpolate(d3.interpolateHcl)
+        .range([d3.rgb(lightGold), d3.rgb(darkGold)]);
+
+    //Simple Scale for Width
+    let wExtent = d3.extent(nodeData, function (d) {
+        return d.w
+    });
+
+    var wScale = d3.scaleLinear()
+        .domain(wExtent)
+        .range([0.35, 0.75]);
+
+    //Simple scale for lineWidth
+    let lineWidthExtent = d3.extent(edgeData, function (d) {
+        return d.weight
+    });
+
+    var lwScale = d3.scaleLinear()
+        .domain(lineWidthExtent)
+        .range([3, 20]);
+
+    scene.append("a-entity")
+        .attr("mixin", "graph-parent")
+        .attr("class", "graph")
+        .attr("id", "process-graph")
+        .attr("position", "0 " + maxHeight / 2 + " " + -radius);
+
+    var graph = scene.select("#process-graph");
+
+    /*var position = graph.append("a-box")
+        .attr("scale", "0.1 0.1 0.1");*/
+
+    var nodes = graph.selectAll("#nodes")
+        .data(nodeData)
+        .enter()
+        .append("a-entity")
+        .attr("mixin", "node-parent")
+        .attr("class", "node-parent")
+        .attr("id", d => ('p' + d.nodeid))
+        .attr("position", function (d, index) {
+            connectedTails[index] = edges.filter(function (eD) {
+                return eD.tail == d.name
+            });
+            connectedHeads[index] = edges.filter(function (eD) {
+                return eD.head == d.name
+            });
+            if (curve) {
+                let curvyCoords = getCurvyCoords(pScale(d.x), tScale(d.y), radius, eyeHeight)
+                return curvyCoords[0] + ' ' + curvyCoords[1] + ' ' + curvyCoords[2]
+            }
+            return xScale(d.x) + ' ' + yScale(d.y) + ' 0'
+        }).append("a-entity")
+        .attr("mixin", "node-element")
+        .attr("class", "node")
+        .attr("id", d => ('n' + d.nodeid))
+        //Normal GEO
+        .attr('geometry', d => ('height: ' + 0.09 + ' ; width: ' + wScale(d.w)))
+        //BEZIER TEST GEO
+        //.attr('geometry', d => ('height: ' + 1 + ' ; width: ' + 1))
+        .attr("material", d => ("color: " + colorBlue(d.weight)))
+        .attr('text', d => ('value: ' + d.name + "; width: 1; color: " + getTextColor(colorBlue(d.weight)) + "; lineHeight: 52;"));
+
+    nodes
+        .on("animationcomplete", function (d) {
+            if (d3.event.detail.name == "animation__selectheight") {
+                d3.select(this).attr("text", d => ("value: " + d.name + "\n Weight: " + d.weight + "\n Frequency: " + d.selected.caseFreq + "\n" + f(d.selected.casePerc) + '%' + ";"))
+            }
+            this.components[d3.event.detail.name].animation.reset();
+        })
+        .on("grab-start", function (d) {
+            d3.select(this).attr("text", d => ("value: ;"))
+        })
+        .on("grab-end", function (d) {
+            d3.select(this).attr("text", d => ("value: " + d.name + ";"))
+        });
+    
+    edges = graph.selectAll("#edges")
+        .data(edgeData)
+        .enter()
+        .append("a-jline")
+        .attr('class', 'cancurve')
+        .attr('class', function (d) {
+            return 'cancurve n' + d.fromnode + ' n' + d.tonode + ' l' + d.fromnode + d.tonode + d.weight
+        })
+        .attr('id', function (d) {
+            return d.head + d.tail
+        })
+        .attr('color', function (d) {
+            //console.log("linewidth should be: " + f(lwScale(d.weight)));
+            return colorBlue(d.weight)
+        })
+        .attr('linewidth', function (d) {
+            //console.log("linewidth should be: " + f(lwScale(d.weight)));
+            return lwScale(d.weight)
+        })
+        .attr('linewidthstyler', function (d) {
+            return "1"
+        })
+        //.attr('linewidth', '100')
+        .attr('path', function (d, index) {
+
+            var curvy_pathString = '';
+            var flat_pathString = '';
+            var tP = [];
+            var p = d.controlpoints;
+            let numberOfCurves = (p.length - 1) / 3;
+
+            for (let i = 0; i < numberOfCurves; i++) {
+                let j = i * 3;
+                var bezier = new THREE.CubicBezierCurve3(
+                    new THREE.Vector3(p[0 + j].x, p[0 + j].y, p[0 + j].z),
+                    new THREE.Vector3(p[1 + j].x, p[1 + j].y, p[1 + j].z),
+                    new THREE.Vector3(p[2 + j].x, p[2 + j].y, p[2 + j].z),
+                    new THREE.Vector3(p[3 + j].x, p[3 + j].y, p[3 + j].z));
+                var points = bezier.getPoints(lineResolution);
+                tP.push(...points);
+            }
+            let curvyCoords = [];
+            for (let i = 0; i < tP.length; i++) {
+                curvyCoords[i] = getCurvyCoords(pScale(tP[i].x), tScale(tP[i].y), radius - zshift, eyeHeight);
+            }
+            for (let i = 0; i < tP.length - 1; i++) {
+                curvy_pathString += curvyCoords[i][0] + ' ' + curvyCoords[i][1] + ' ' + curvyCoords[i][2] + ', ';
+                flat_pathString += xScale(tP[i].x) + ' ' + yScale(tP[i].y) + ' ' + -(zshift) + ', ';
+            }
+            curvy_pathString += curvyCoords[tP.length - 1][0] + ' ' + curvyCoords[tP.length - 1][1] + ' ' + curvyCoords[tP.length - 1][2];
+            flat_pathString += xScale(tP[tP.length - 1].x) + ' ' + yScale(tP[tP.length - 1].y) + ' ' + (-zshift);
+            curvyPaths[index] = curvy_pathString;
+            flatPaths[index] = flat_pathString;
+            if (curve) {
+                return curvyPaths[index]
+            } else {
+                return flatPaths[index]
+            }
+        });
+}
+
+function getCurvyCoords(x, y, radius, eyeHeight) {
+    let xC = -radius * Math.sin(x * (Math.PI / 180)) * Math.cos(y * (Math.PI / 180));
+    let yC = eyeHeight + radius * Math.sin(y * (Math.PI / 180));
+    let zC = -radius * Math.cos(x * (Math.PI / 180)) * Math.cos(y * (Math.PI / 180));
+    let curvyCoords = [xC, yC, zC];
+    return curvyCoords
+}
+
+function getTextColor(c) {
+    let color = d3.rgb(c);
+    //console.log(color);
+    let r = color.r;
+    let g = color.g;
+    let b = color.b;
+    if ((r + g + b) > 500) {
+        //console.log("true");
+        return "#000000"
+    } else {
+        //console.log("false");
+        return "#ffffff"
+    }
+}
---
+++ Pg/utility-components.js
@@ -0,0 +1,201 @@
+var debug = AFRAME.utils.debug;
+var coordinates = AFRAME.utils.coordinates;
+
+var warn = debug('components:look-at-fixed-height:warn');
+var isCoordinates = coordinates.isCoordinates;
+
+delete AFRAME.components['look-at-fixed-height'];
+
+/**
+ * Look-at component.
+ *
+ * Modifies rotation to either track another entity OR do a one-time turn towards a position
+ * vector.
+ *
+ * If tracking an object via setting the component value via a selector, look-at will register
+ * a behavior to the scene to update rotation on every tick.
+ */
+AFRAME.registerComponent('look-at-fixed-height', {
+    schema: {
+        default: '',
+
+        parse: function (value) {
+            // A static position to look at.
+            if (isCoordinates(value) || typeof value === 'object') {
+                return coordinates.parse(value);
+            }
+            // A selector to a target entity.
+            return value;
+        },
+
+        stringify: function (data) {
+            if (typeof data === 'object') {
+                //console.log(data);
+                return coordinates.stringify(data);
+            }
+            //console.log(data);
+            return data;
+        }
+    },
+
+    init: function () {
+        this.target3D = null;
+        this.vector = new THREE.Vector3();
+    },
+
+    /**
+     * If tracking an object, this will be called on every tick.
+     * If looking at a position vector, this will only be called once (until further updates).
+     */
+    update: function () {
+        var self = this;
+        var target = self.data;
+        var object3D = self.el.object3D;
+        var targetEl;
+
+        // No longer looking at anything (i.e., look-at="").
+        if (!target || (typeof target === 'object' && !Object.keys(target).length)) {
+            return self.remove();
+        }
+
+        // Look at a position.
+        if (typeof target === 'object') {
+            return object3D.lookAt(new THREE.Vector3(target.x, target.y, target.z));
+        }
+
+        // Assume target is a string.
+        // Query for the element, grab its object3D, then register a behavior on the scene to
+        // track the target on every tick.
+        targetEl = self.el.sceneEl.querySelector(target);
+        if (!targetEl) {
+            warn('"' + target + '" does not point to a valid entity to look-at');
+            return;
+        }
+        if (!targetEl.hasLoaded) {
+            return targetEl.addEventListener('loaded', function () {
+                self.beginTracking(targetEl);
+            });
+        }
+        return self.beginTracking(targetEl);
+    },
+
+    tick: (function () {
+        var vec3 = new THREE.Vector3();
+
+        return function (t) {
+            // Track target object position. Depends on parent object keeping global transforms up
+            // to state with updateMatrixWorld(). In practice, this is handled by the renderer.
+            var target;
+            var target3D = this.target3D;
+            var object3D = this.el.object3D;
+            var vector = this.vector;
+
+            if (target3D) {
+                object3D.parent.worldToLocal(target3D.getWorldPosition(vec3));
+                if (this.el.getObject3D('camera')) {
+                    // Flip the vector to -z, looking away from target for camera entities. When using
+                    // lookat from THREE camera objects, this is applied for you, but since the camera is
+                    // nested into a Object3D, we need to apply this manually.
+                    vector.subVectors(object3D.position, vec3).add(object3D.position);
+                } else {
+                    vector = vec3;
+                }
+                vector.y = object3D.position.y;
+                object3D.lookAt(vector);
+            }
+        };
+    })(),
+
+    beginTracking: function (targetEl) {
+        this.target3D = targetEl.object3D;
+    }
+});
+
+var warn = debug('components:dynamic-positionality');
+var isCoordinates = coordinates.isCoordinates;
+
+delete AFRAME.components['dynamic-positionality'];
+
+/**
+ * Look-at component.
+ *
+ * Modifies rotation to either track another entity OR do a one-time turn towards a position
+ * vector.
+ *
+ * If tracking an object via setting the component value via a selector, look-at will register
+ * a behavior to the scene to update rotation on every tick.
+ */
+AFRAME.registerComponent('dynamic-positionality', {
+    schema: {
+        radius: {type: "float"}
+    },
+    init: function (){
+        this.target3D = null;
+        this.trackingVector = new THREE.Vector3();
+        this.worldVector = new THREE.Vector3();
+    },
+    update: function (){
+        var self = this;
+        var targetEl = self.el.sceneEl.querySelector('[camera]');   
+        if (!targetEl) {
+            warn('"' + target + '" does not point to a valid entity to look-at');
+            return;
+        }
+        if (!targetEl.hasLoaded) {
+            return targetEl.addEventListener('loaded', function () {
+                self.beginTracking(targetEl);
+            });
+        }
+        return self.beginTracking(targetEl);
+    },
+    tick: (function (){
+        return function (t) {
+            // Track target object position. Depends on parent object keeping global transforms up
+            // to state with updateMatrixWorld(). In practice, this is handled by the renderer.
+            var target;
+            var target3D = this.target3D;
+            var object3D = this.el.object3D;
+            var parentPosition = object3D.parent.position;
+            var trackingVector = this.trackingVector;
+            var worldVector = this.worldVector
+
+            if (target3D) {
+                object3D.parent.worldToLocal(target3D.getWorldPosition(worldVector));
+                trackingVector = worldVector;
+                var distance = parentPosition.distanceTo(trackingVector);
+                var desiredZ = THREE.Math.clamp((1-distance), 0, 1);
+                console.log(-desiredZ);
+                object3D.position.z = -desiredZ;
+            }
+        }; 
+    })(),
+    beginTracking: function (targetEl) {
+        this.target3D = targetEl.object3D;
+    }
+});
+
+AFRAME.registerComponent('selectable', {
+    schema: {
+        state: {
+            type: "boolean"
+        }
+    },
+    init: function () {
+        var self = this;
+        this.initialPos = self.el.getAttribute('position');
+    },
+    update: function (oldData) {
+
+        var data = this.data;
+        var el = this.el;
+
+        if (data.state !== oldData.state) {
+            if (data.state) {
+                el.object3D.position.z = 0.1;
+            } else {
+                el.object3D.position = this.initialPos;
+            }
+        }
+    }
+
+});
